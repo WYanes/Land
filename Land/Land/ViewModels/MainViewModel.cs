3148e41bc08712534a9fcb4bb210f3597f765672
WYanes <60592826+WYanes@users.noreply.github.com>
2020-02-06 15:30:41 -0500

    Create MainViewModel.cs

---
+++ Land/Land/ViewModels/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Land.ViewModels
+{
+    public class MainViewModel
+    {
+        #region View Models
+        public LoginViewModel Login
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
+        #endregion
+    }
+}
