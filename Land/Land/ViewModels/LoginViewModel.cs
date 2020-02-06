cf4430cb31ee8cb584b4f29b78a1a550fa50047a
WYanes <60592826+WYanes@users.noreply.github.com>
2020-02-06 16:41:06 -0500

    Update LoginViewModel.cs

--- Land/Land/ViewModels/LoginViewModel.cs
+++ Land/Land/ViewModels/LoginViewModel.cs
@@ -3,11 +3,27 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace Land.ViewModels
-{ using Xamarin.Forms;
+{
+    using Xamarin.Forms;
     using GalaSoft.MvvmLight.Command;
     using System.Windows.Input;
-    public class LoginViewModel
+    using System.ComponentModel;
+
+    public class LoginViewModel : INotifyPropertyChanged
     {
+        #region Events
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        #region Attributes
+        private string email;
+        private string password;
+        private bool isRunning;
+        private bool isEnabled;
+        #endregion
+
         #region Properties
         public string Email
         {
@@ -23,27 +39,36 @@ namespace Land.ViewModels
         public bool IsRunning { get; set; }
 
         public bool IsRemembered { get; set; }
+
+        public bool IsEnabled
+        {
+            get;
+            set;
+        }
         #endregion
 
         #region Constructors
         public LoginViewModel()
         {
             this.IsRemembered = true;
+            this.IsEnabled = true;
         }
         #endregion
 
         #region Commands
-        public ICommand LoginCommand {
+        public ICommand LoginCommand
+        {
             get
             {
                 return new RelayCommand(Login);
-            } 
-             
+            }
+
         }
+
 
         private async void Login()
         {
-            if(string.IsNullOrEmpty(this.Email))
+            if (string.IsNullOrEmpty(this.Email))
             {
                 await Application.Current.MainPage.DisplayAlert("Error",
                     "You must enter a email.",
@@ -56,9 +81,21 @@ namespace Land.ViewModels
                 await Application.Current.MainPage.DisplayAlert("Error",
                     "You must enter a Password.",
                     "Accept");
+                this.Password = string.Empty;
                 return;
             }
 
+            if (this.Email != "willianguitarflash6@gmail.com" || this.Password != "2345")
+            {
+                await Application.Current.MainPage.DisplayAlert("Error",
+                    "Error email or password incorrect",
+                    "Accept");
+                return;
+            }
+
+            await Application.Current.MainPage.DisplayAlert("OK",
+                    "Jejejej",
+                    "Accept");
         }
         #endregion
     }
