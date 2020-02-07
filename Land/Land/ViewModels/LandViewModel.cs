7535305a01b981551d3caffd601b70038c7c0dcc
WYanes <60592826+WYanes@users.noreply.github.com>
2020-02-07 14:09:32 -0500

    Conectando la Mainview Model con la LandPage

---
+++ Land/Land/ViewModels/LandViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Land.ViewModels
+{
+    public class LandViewModel : ContentPage
+    {
+        public LandViewModel()
+        {
+        }
+    }
+}
---
+++ Land/Land/Views/LandPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Land.Views
+{
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LandPage : ContentPage
+    {
+        public LandPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
